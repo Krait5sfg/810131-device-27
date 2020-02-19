50e36a01fc197b3c0b39b633e7cce676586b0f8c
Yura <yurii_kulachenko@mail.ru>
2020-02-19 22:49:34 +0300

    работает js на ie 11, всплывающее меня в ie 11, убрал прозрачный отступ между кнопкой и тенью в блоке сервисов

--- js/script.js
+++ js/script.js
@@ -28,13 +28,18 @@ catalogBtn.addEventListener('mouseenter', function () {
     });
 });
 
+//код чтобы в IE 11 работал forEach
+if (typeof NodeList.prototype.forEach !== 'function') {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 //переключение слайдов
 const sliders = document.querySelectorAll('.promo-slide');
 const slideButtons = document.querySelectorAll('.promo-slide-toggle-button');
-slideButtons.forEach(element => {
+slideButtons.forEach(function (element) {
     element.addEventListener('click', function () {
         let dataSlide = element.getAttribute('data-slide');
-        sliders.forEach(element => {
+        sliders.forEach(function (element) {
             if (dataSlide === element.getAttribute('data-slide')) {
                 element.classList.remove('hidden');
             } else {
@@ -47,17 +52,17 @@ slideButtons.forEach(element => {
 //кнопки для блоков сервисов, перелкючение слайдов сервисов
 const serviceSliders = document.querySelectorAll('.service-slide');
 const serviceButtons = document.querySelectorAll('.button.service-button');
-serviceButtons.forEach(element => {
+serviceButtons.forEach(function (element) {
     element.addEventListener('click', function () {
         let dataSlide = element.getAttribute('data-slide');
-        serviceSliders.forEach(element => {
+        serviceSliders.forEach(function (element) {
             if (dataSlide === element.getAttribute('data-slide')) {
                 element.classList.remove('hidden');
             } else {
                 element.classList.add('hidden');
             }
         });
-        serviceButtons.forEach(element => {
+        serviceButtons.forEach(function (element) {
             element.classList.remove('active');
         });
         element.classList.add('active');
@@ -69,18 +74,18 @@ const modalUser = document.querySelector('.modal-user');
 btnModalUser = [];
 btnModalUser.push(document.querySelector('.button.button-contacts'));
 btnModalUser.push(document.querySelector('.button-modal-close'));
-btnModalUser.forEach(element => {
+btnModalUser.forEach(function (element) {
     element.addEventListener('click', function () {
         modalUser.classList.toggle('hidden');
     });
 });
 
-// модальное окно с картой
+// // модальное окно с картой
 const modalMap = document.querySelector('.modal-map');
 const btnModalMap = [];
 btnModalMap.push(document.querySelector('.contancts-image-box'));
 btnModalMap.push(document.querySelector('.button-close-modal-map'));
-btnModalMap.forEach(element => {
+btnModalMap.forEach(function (element) {
     element.addEventListener('click', function () {
         modalMap.classList.toggle('hidden');
     });
